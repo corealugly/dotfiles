511bd8298dcc844d70bf87340f1681f241bf29a8
alexey <corealugly@gmail.com>
2018-07-29 12:39:38 +0300

    add fonts, add encrypted alias info, change shading in urxvt

--- zsh_custom/alias.zsh
+++ zsh_custom/alias.zsh
@@ -1,2 +1,8 @@
 alias telegramh="telegram -many -workdir ~/.telegram/home/"
 alias telegramw="telegram -many -workdir ~/.telegram/work/"
+
+alias DRM="docker run --entrypoint /bin/bash --rm -it"
+alias DR="docker run --entrypoint /bin/bash -it"
+alias CAD="cd /etc/pki/ca/" 
+
+alias XRANDWORK="xrandr --output eDP1 --auto --left-of HDMI1"
