841f87a8644d1cf94a59ae890580a51c8d742b61
alex <corealugly@gmail.com>
2018-09-09 18:23:23 +0300

    update gpg-agent.plugin.zsh

--- zsh_custom/plugins/gpg-agent/gpg-agent.plugin.zsh
+++ zsh_custom/plugins/gpg-agent/gpg-agent.plugin.zsh
@@ -1,8 +1,10 @@
 # Enable gpg-agent if it is not running
 GPG_AGENT_SOCKET="$(gpgconf --list-dirs | grep 'agent-ssh-socket' | cut -d : -f 2)"
-#GPG_AGENT_SOCKET="${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh"
-if [ ! -S ${GPG_AGENT_SOCKET} ]; then
-  gpg-agent --daemon >/dev/null 2>&1
+#GPG_AGENT_DIRMNGR_SOCKET="$(gpgconf --list-dirs | grep 'agent-socket' | cut -d : -f 2)"
+pnum="$(pgrep gpg-agent)"
+if [ ! -S "${GPG_AGENT_SOCKET}" ] && [ -z "${pnum}" ]; then
+  gpg-connect-agent updatestartuptty /bye >/dev/null 2>&1
+  #gpg-agent --daemon >/dev/null 2>&1
 fi
 
 export GPG_TTY=$(tty)
@@ -12,4 +14,5 @@ GNUPGCONFIG="${GNUPGHOME:-"$HOME/.gnupg"}/gpg-agent.conf"
 if [ -r "${GNUPGCONFIG}" ] && grep -q enable-ssh-support "${GNUPGCONFIG}"; then
   unset SSH_AGENT_PID
   export SSH_AUTH_SOCK=${GPG_AGENT_SOCKET}
+  gpg-connect-agent updatestartuptty /bye 
 fi
