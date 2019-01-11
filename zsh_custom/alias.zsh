5a483e3ec8e803ebca731cef99c96cbc7934b1eb
alex <corealugly@gmail.com>
2019-01-11 10:21:48 +0300

    fix xrand alias

--- zsh_custom/alias.zsh
+++ zsh_custom/alias.zsh
@@ -9,7 +9,9 @@ alias gpg-reload="gpg-connect-agent reloadagent /bye"
 
 alias lshl="ls -l \$(find ./ -maxdepth 2 -type l -print)"
 
-alias xrhome="xrandr --output eDP-1 --off --output DP-3-1 --auto --pos 2560x0 --output DP-3-2  --primary --auto --pos 0x0"
+alias xrhome="xrandr --output eDP-1 --off --output DP-3-1 --auto --pos 2560x0 --rotate normal --output DP-3-2  --primary --auto --pos 0x0 --rotate normal" 
+alias xrhomeleft="xrandr --output eDP-1 --off --output DP-3-1 --auto --pos 2560x0 --rotate left --output DP-3-2 --primary --auto --pos 0x0"
+alias xrnorm="xrandr --output eDP-1 --auto --output DP-3-1 --off --output DP-3-2 --off"
 alias xrwork="xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 1920x0 --rotate normal --primary"
 
 alias pbcopy='xsel --clipboard --input'
